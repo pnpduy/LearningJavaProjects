656d017a0f5444eb6a0d66981420d6ef85ecdfd4
Pham Nguyen Phuong Duy <45529371+pnpduy@users.noreply.github.com>
2024-03-23 20:36:41 +0700

    Add files via upload

---
+++ PositiveNegativeOrZero/src/Main.java
@@ -0,0 +1,11 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        PositiveNegativeZero.checkNumber(33275);
+        PositiveNegativeZero.checkNumber(1);
+        PositiveNegativeZero.checkNumber(-1);
+        PositiveNegativeZero.checkNumber(0);
+    }
+}
