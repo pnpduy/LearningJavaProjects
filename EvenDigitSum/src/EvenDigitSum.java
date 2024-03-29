ddb6a2eef21c8a9330be02e97088342f1c02ff10
Pham Nguyen Phuong Duy <45529371+pnpduy@users.noreply.github.com>
2024-03-29 14:14:19 +0700

    Add files via upload

---
+++ EvenDigitSum/src/EvenDigitSum.java
@@ -0,0 +1,17 @@
+public class EvenDigitSum {
+    // write code here
+    public static int getEvenDigitSum(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int sum = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            if (digit % 2 == 0) {
+                sum += digit;
+            }
+            number /= 10;
+        }
+        return sum;
+    }
+}
