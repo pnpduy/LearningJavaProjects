a39ae2e2d8c0ab7e1253fd7e078fde94014e90da
Pham Nguyen Phuong Duy <45529371+pnpduy@users.noreply.github.com>
2024-03-29 14:14:00 +0700

    Add files via upload

---
+++ FirstLastDigitSum/src/FirstLastDigitSum.java
@@ -0,0 +1,13 @@
+public class FirstLastDigitSum {
+    // write code here
+    public static int sumFirstAndLastDigit(int number) {
+        if (number < 0) {
+            return -1;
+        }
+        int lastDigit = number % 10;
+        while (number >= 10) {
+            number /= 10;
+        }
+        return number + lastDigit;
+    }
+}
